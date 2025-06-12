b94b135ece5145ff7b5e7499a6aed504a1d92256
Jay J <sarfojanet930@gmail.com>
2025-06-12 23:10:04 +0000

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,35 @@
+let age = 25;
+let year = 2020;
+
+console.log(age, year);
+
+age = 30;
+console.log(age); 
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+score =  80;
+console.log(score);
+
+//string concatonation
+let firstName = "Janette";
+let lastName = "Sarfo";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//string extraction
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+
+//argument
+let email = "sarfojanet930@gmail.com";
+let index = email.indexOf("@");
+console.log(index);
